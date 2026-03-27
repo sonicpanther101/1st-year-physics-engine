37f2119bc1188dba67c5563da7b6ef84a4ba52c5
sonicpanther101 <sonicpanther101@gmail.com>
2026-03-28 11:40:56 +1300

    Rendering working on nix using learnopengl multiple lights stage

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+	description = "1st-year-physics-engine";
+
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+		flake-utils.url = "github:numtide/flake-utils";
+	};
+
+	outputs = { self, nixpkgs, flake-utils }:
+	    flake-utils.lib.eachDefaultSystem (system:
+			let
+				pkgs = nixpkgs.legacyPackages.${system};
+			in {
+				packages.default = pkgs.stdenv.mkDerivation {
+					pname = "1st-year-physics-engine";
+					version = "0.1.0";
+
+					src = ./src;
+
+					nativeBuildInputs = with pkgs; [
+						pkg-config
+					];
+
+					buildInputs = with pkgs; [
+						libGL
+						glfw
+						glew
+						glm
+						assimp
+						imgui
+						stb
+					];
+
+					buildPhase = ''
+						# Collect all .cpp files in src/
+						SOURCES=$(find . -name "*.cpp" | tr '\n' ' ')
+
+						$CXX $SOURCES \
+							-std=c++17 \
+							-O2 \
+							-D_CRT_SECURE_NO_WARNINGS \
+							$(pkg-config --cflags --libs glfw3 glew assimp) \
+							-I${pkgs.imgui}/include \
+							-I${pkgs.imgui}/include/imgui \
+							-I${pkgs.stb}/include/stb \
+							$(pkg-config --cflags --libs imgui 2>/dev/null || true) \
+							-lGL \
+							-o physics-engine
+					'';
+
+					installPhase = ''
+						mkdir -p $out/bin
+						cp physics-engine $out/bin/
+					'';
+				};
+
+				devShells.default = pkgs.mkShell {
+					inputsFrom = [ self.packages.${system}.default ];
+					packages = with pkgs; [
+						gdb
+						clang-tools
+					];
+				};
+			}
+		);
+}
